64cc9093dfea49731d1c48387dd9a5a7b187b58e
joshi <joseph.ilagan.99.com>
2023-11-04 10:51:43 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Task from './task';
+
 
 function App() {
+  const [toDoList, setToDoList] = useState([])
+  const [newTask, setNewTask] = useState("")
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    const task = {
+      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    }
+
+    //push the task variable to the todoList using spread operator
+    setToDoList([...toDoList, task])
+  }
+
+  const colorize = (id) =>{
+    const comp = toDoList.map((task) => {
+      if (task.id === id){
+        if ( task.completed ){
+          //modify the "completed" value of the current index
+          //using spread operator
+          return {...task, completed: false};
+        } else {
+        return {...task, completed: true};
+        }
+      } else {
+        return task
+      }
+    })
+    setToDoList(comp)
+  }
+  const dels = (id) => {   
+    const newTodoList = toDoList.filter((task) => {
+      // if (tasks === taskName){
+      //   return false
+      // } else {
+      //   return true
+      // }
+      // ********** using just "not strict equality" operator
+      return task.id !== id;
+    })
+  setToDoList(newTodoList)
+  }
+
+  const clearToDo = () => {
+    setToDoList([])
+  }
+
+  // ************   FOR HTML   *****************
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-container">
+    <header>   
+       <p>Create a To do List</p>
+    </header>
+    <div id="item-form">
+        <div className="inputs-buttons-div">
+            <input             
+            className="form-input"            
+            placeholder="Enter To Do Item"
+            onChange={handleChange}
+            />         
+        </div>
+        <div className="inputs-buttons-div">
+            <button type="submit" className="toDoBtn" onClick={addTask
+            }>Add To Do</button>
+            <button type="submit" className="toDoBtn">Search To Do</button>
+            <button id="clear" onClick={clearToDo} className="toDoBtn">Clear To Do List</button>
+        </div>
+    </div>
+    <ul className="item-list">
+    {toDoList.map((task, key)=>{
+              return (
+                <Task key={key}
+                taskName={task.taskName} 
+                id={task.id} 
+                completed={task.completed}
+                dels={dels}
+                colorize={colorize}
+                />
+              )
+                      
+            })}
+      </ul>
     </div>
   );
 }
---
+++ src/task.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Task = (props) => {
+  return (
+    <li  style={{ backgroundColor: props.completed ? "green" : "#92730c"}}>
+         <p>{props.taskName}</p>
+          <div className="complete-delete-container">
+            <button className="complete-delete save-button btn" onClick={() => props.colorize(props.id)}>
+              <span className="completed">Completed</span>
+            </button>
+            <button className="complete-delete btn" onClick={() => props.dels(props.id)}>
+              <span className="delete">Delete</span>
+            </button>
+          </div>  
+        </li>
+  )
+}
+
+export default Task
